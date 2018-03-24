58d5343177f87dfdc37e4422c5f84963b6a3a61d
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-03-24 13:02:34 -0700

    figure updates

--- R/04_k-temp-figures-biomass.R
+++ R/04_k-temp-figures-biomass.R
@@ -19,6 +19,7 @@ flux_rates <- flux_rates_raw %>%
 	mutate(flux_type = str_replace(flux_type, "respiration_corr", "respiration")) %>% 
 	mutate(inverse_temp = (1/(.00008617*(temperature.x+273.15))))
 
+
 # tt_mass <- tt %>% 
 # 	mutate(cell_biomass_MD = 0.3584378*(cell_volume)^1.088) %>% 
 # 	mutate(cell_biomass_R = 0.47*(cell_volume)^0.99) %>% 
@@ -32,6 +33,10 @@ flux2 <- flux_rates %>%
 	mutate(rate_biomassR = 0.47*(rate_estimate)^0.99) %>% 
 	mutate(rate_biomassM = 0.109*(rate_estimate)^0.991)
 
+
+flux2 %>% 
+	group_by(temperature.y) %>% 
+	distinct(well_id) %>% tally() %>% View
 
 respiration_plot_menden <- flux2 %>% 
 	filter(flux_type == "respiration") %>% 
@@ -95,12 +100,13 @@ photosynthesis_plot_reynolds <- flux2 %>%
 
 respiration_plot_montagnes <- flux2 %>% 
 	filter(flux_type == "respiration") %>% 
-	ggplot(aes(x = inverse_temp, y = log(rate_biomassM))) + 
-	geom_point(size = 4, alpha = 0.5) + 
+	ggplot(aes(x = inverse_temp, y = log(rate_biomassM))) +
 	geom_smooth(method = "lm", size =2, color = "black") +
+	geom_point(size = 4, alpha = 0.2) + 
+	geom_point(size = 4, shape = 1) + 
 	# facet_wrap( ~ flux_type, scales = "free") +
 	theme_bw() +
-	ylab("Ln oxygen flux (mg O2 / ug C / hr) ") +
+	ylab(bquote('ln oyxygen flux ('*mg ~O[2]*' ug '*C^-1~hr^-1*')')) +
 	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 				panel.background = element_blank(), axis.line = element_line(colour = "black")) +
 	theme(text = element_text(size=15, family = "Helvetica")) +
@@ -111,17 +117,22 @@ respiration_plot_montagnes <- flux2 %>%
 photosynthesis_plot_montagnes <- flux2 %>% 
 	filter(flux_type == "gross photosynthesis") %>% 
 	ggplot(aes(x = inverse_temp, y = log(rate_biomassM))) + 
-	geom_point(size = 4, alpha = 0.5) + 
 	geom_smooth(method = "lm", size =2, color = "black") +
+	geom_point(size = 4, alpha = 0.2) + 
+	geom_point(size = 4, shape = 1) + 
 	# facet_wrap( ~ flux_type, scales = "free") +
 	theme_bw() +
-	ylab("Ln oxygen flux (mg O2 / ug C / hr) ") +
+	ylab(bquote('ln oyxygen flux ('*mg ~O[2]*' ug '*C^-1~hr^-1*')')) +
 	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 				panel.background = element_blank(), axis.line = element_line(colour = "black")) +
 	theme(text = element_text(size=15, family = "Helvetica")) +
 	scale_x_reverse(sec.axis = sec_axis(~((1/(.*8.62 * 10^(-5)))-273.15))) + xlab("Temperature (1/kT)") + ggtitle("Temperature (°C)") +
 	theme(plot.title = element_text(hjust = 0.5, size = 14)) +
 	geom_point(size = 4, shape = 1, color = "black") 
+
+fig1_montagnes <- plot_grid(photosynthesis_plot_montagnes, respiration_plot_montagnes, labels = c("A) Photosynthesis", "B) Respiration"), label_fontface = "plain", ncol = 2, nrow = 1, label_x = 0, hjust = 0)
+save_plot("figures/k-temp-figure1-montagnes.pdf", fig1_montagnes, nrow = 1, ncol = 2, base_height = 4, base_width = 4.4)
+
 
 
 fig1_biomass <- plot_grid(photosynthesis_plot_reynolds, respiration_plot_reynolds,photosynthesis_plot_menden, respiration_plot_menden,photosynthesis_plot_montagnes, respiration_plot_montagnes, labels = c("A) Photosynthesis", "B) Respiration"), label_fontface = "plain", ncol = 2, nrow = 3, label_x = 0, hjust = 0)
@@ -142,6 +153,16 @@ flux2 %>%
 	filter(rate_estimate > 0) %>% 
 	group_by(flux_type) %>% 
 	do(tidy(lm(log(rate_biomassM) ~ inverse_temp, data = .), conf.int = TRUE)) %>% View
+
+flux2 %>% 
+	filter(rate_estimate > 0) %>% 
+	filter(flux_type == "gross photosynthesis") %>% 
+	lm(log(rate_biomassM) ~ inverse_temp, data = .) %>% summary()
+
+flux2 %>% 
+	filter(rate_estimate > 0) %>% 
+	filter(flux_type == "respiration") %>% 
+	lm(log(rate_biomassM) ~ inverse_temp, data = .) %>% summary()
 
 
 # CUE ---------------------------------------------------------------------
